1af73c572e934952d8cf51fb301ae85212c8db2e
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-12 15:44:44 -0500

    renamed stuff, added sass compiler

---
+++ client/src/containers/Register.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
+import "../css/login.scss";
+
+export default function Register() {
+
+	let history = useHistory();
+
+	const loginBtn = () => {
+		history.push("./login");
+	};
+
+	return (
+		<div className="register">
+			<Button onClick={loginBtn}>
+				Login
+			</Button>
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicName">
+					<Form.Label>Name</Form.Label>
+					<Form.Control type="name" placeholder="Enter name" />
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" />
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+
+				<Button block size="lg" type="submit">
+					Register
+				</Button>
+			</Form>
+		</div>
+	);
+}
