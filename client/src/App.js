f89379288819e598d9f8ecf622da8eb4de47fe23
Farooq <syed@farooq.dev>
2021-10-11 19:34:48 -0500

    Created login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,13 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Router from './Router/Router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-    state = {
-        data: null
-    };
-
-    componentDidMount() {
-        this.callBackendAPI()
-            .then(res => this.setState({data: res.express}))
-            .catch(err => console.log(err));
-    }
-
-    // fetching the GET route from the Express server which matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('/hello');
-        const body = await response.json();
-
-        if (response.status !== 200) {
-            throw Error(body.message)
-        }
-        return body;
-    };
-
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">{this.state.data}</h1>
-                </header>
-            </div>
-        );
+      return (      
+        <Router />
+      );
     }
-}
+  }
 
 export default App;
