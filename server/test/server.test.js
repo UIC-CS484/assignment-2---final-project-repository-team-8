d97eae0d66551f74d291fc1877f6cfe091ea9095
Farooq <syed@farooq.dev>
2021-10-15 21:29:59 -0500

    Deleted useless import

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -2,7 +2,6 @@ const app = require('../server');
 const supertest = require('supertest');
 var sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database('db-test.sqlite');
-const query = require("../query");
 
 beforeAll(() => {
   process.env.NODE_ENV = 'test';
