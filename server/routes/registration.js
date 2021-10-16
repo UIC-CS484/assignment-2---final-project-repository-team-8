00aabddaf86382319dab85bd145451f78b0035ea
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-16 12:31:51 -0500

    nit

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -1,4 +1,3 @@
-const {param} = require("express/lib/router");
 const EMAIL_FORMAT = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_FORMAT = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{5,}$/;
 
@@ -17,19 +16,19 @@ const PASSWORD_FORMAT = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{5,}$/;
  * @returns {(boolean|string)[]} whether or this is a valid combination of registration parameters
  */
 function validRegistrationParameters(name, email, password) {
-    const parametersArePresent = !!name && !!email && !!password;
-    const emailIsValid = EMAIL_FORMAT.test(email);
-    const passwordIsValid = PASSWORD_FORMAT.test(password);
+	const parametersArePresent = !!name && !!email && !!password;
+	const emailIsValid = EMAIL_FORMAT.test(email);
+	const passwordIsValid = PASSWORD_FORMAT.test(password);
 
-    if (!parametersArePresent) {
-        return [false, "Please provide a name, email, and password"]
-    } else if (!emailIsValid) {
-        return [false, "Please provide a valid email"]
-    } else if (!passwordIsValid) {
-        return [false, "Please provide a valid password"]
-    }
+	if (!parametersArePresent) {
+		return [false, "Please provide a name, email, and password"];
+	} else if (!emailIsValid) {
+		return [false, "Please provide a valid email"];
+	} else if (!passwordIsValid) {
+		return [false, "Please provide a valid password"];
+	}
 
-    return [true, null]
+	return [true, null];
 }
 
 module.exports = validRegistrationParameters;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-var sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -7,19 +7,19 @@ const passport = require("passport");
 const StatusCodes = require("http-status-codes").StatusCodes;
 const validRegistrationParameters = require("./routes/registration");
 const query = require("./query");
-const app = express(); 
+const app = express();
 const saltRounds = 10;
-const port = process.env.NODE_ENV === 'test' ? 8081 : 8080;
-let db = process.env.NODE_ENV === 'test' ? new sqlite3.Database('db-test.sqlite') : require("./database.js");
+const port = process.env.NODE_ENV === "test" ? 8081 : 8080;
+let db = process.env.NODE_ENV === "test" ? new sqlite3.Database("db-test.sqlite") : require("./database.js");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(
-    session({
-        secret: "secretcode",
-        resave: true,
-        saveUninitialized: true
-    })
+	session({
+		secret: "secretcode",
+		resave: true,
+		saveUninitialized: true
+	})
 );
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
@@ -27,82 +27,82 @@ app.use(passport.session());
 require("./passportConf")(passport);
 
 app.listen(port, () => {
-    console.log("Server running on port " + port);
+	console.log("Server running on port " + port);
 });
 
 const error = {
-    BAD_REQUEST: "Please send a properly-formed request!",
-    UNEXPECTED_ERROR: "Unexpected error. Please try again!",
-    USER_NOT_FOUND: "User not found...",
-    EMAIL_ALREADY_EXISTS: "Email already exists!",
-    INCORRECT_PASSWORD: "Passwords do not match!"
+	BAD_REQUEST: "Please send a properly-formed request!",
+	UNEXPECTED_ERROR: "Unexpected error. Please try again!",
+	USER_NOT_FOUND: "User not found...",
+	EMAIL_ALREADY_EXISTS: "Email already exists!",
+	INCORRECT_PASSWORD: "Passwords do not match!"
 };
 
 app.post("/account/register", async (req, res) => {
-    const [ok, err] = validRegistrationParameters(req.body.name, req.body.email, req.body.password)
-    if (!ok) {
-        console.log(err)
-        res.status(StatusCodes.BAD_REQUEST).json({"error": err});
-        return;
-    }
+	const [ok, err] = validRegistrationParameters(req.body.name, req.body.email, req.body.password);
+	if (!ok) {
+		console.log(err);
+		res.status(StatusCodes.BAD_REQUEST).json({ "error": err });
+		return;
+	}
 
-    bcrypt.hash(req.body.password, saltRounds, function (hashErr, hash) {
-        if (hashErr) {
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({"message": "Unable to register user. Please try again!"});
-            return;
-        }
+	bcrypt.hash(req.body.password, saltRounds, function(hashErr, hash) {
+		if (hashErr) {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ "message": "Unable to register user. Please try again!" });
+			return;
+		}
 
-        const params = [req.body.name, req.body.email, hash];
-        db.run(query.INSERT_ACCOUNT, params, (dbErr, row) => {
-            if (dbErr) {
-                res.status(StatusCodes.CONFLICT).json({"error": error.EMAIL_ALREADY_EXISTS});
-            } else {
-                res.sendStatus(StatusCodes.OK);
-            }
-        });
-    });
+		const params = [req.body.name, req.body.email, hash];
+		db.run(query.INSERT_ACCOUNT, params, (dbErr, row) => {
+			if (dbErr) {
+				res.status(StatusCodes.CONFLICT).json({ "error": error.EMAIL_ALREADY_EXISTS });
+			} else {
+				res.sendStatus(StatusCodes.OK);
+			}
+		});
+	});
 });
 
 
 app.post("/account/login", async (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-        if (err) throw err;
-        if (!user) res.send("No User Exists");
-        else {
-            req.logIn(user, (err) => {
-                if (err) throw err;
-                res.send("Successfully Authenticated");
-            });
-        }
-    })(req, res, next);
+	passport.authenticate("local", (err, user, info) => {
+		if (err) throw err;
+		if (!user) res.send("No User Exists");
+		else {
+			req.logIn(user, (err) => {
+				if (err) throw err;
+				res.send("Successfully Authenticated");
+			});
+		}
+	})(req, res, next);
 });
 
 app.get("/account", (req, res) => {
-    if (!req.query.email) {
-        res.status(StatusCodes.BAD_REQUEST).json({"error": error.BAD_REQUEST});
-    }
+	if (!req.query.email) {
+		res.status(StatusCodes.BAD_REQUEST).json({ "error": error.BAD_REQUEST });
+	}
 
-    const params = [req.query.email];
-    db.get(query.GET_ACCOUNT, params, function (err, row) {
-        if (err) {
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({"error": err.message});
-            return console.error(err);
-        }
+	const params = [req.query.email];
+	db.get(query.GET_ACCOUNT, params, function(err, row) {
+		if (err) {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ "error": err.message });
+			return console.error(err);
+		}
 
-        if (row) {
-            res.status(StatusCodes.OK).json(row);
-        } else {
-            res.status(StatusCodes.NOT_FOUND).json({
-                "message": error.USER_NOT_FOUND
-            });
-        }
-    });
+		if (row) {
+			res.status(StatusCodes.OK).json(row);
+		} else {
+			res.status(StatusCodes.NOT_FOUND).json({
+				"message": error.USER_NOT_FOUND
+			});
+		}
+	});
 });
 
 const name = ["Farooq", "Matt"];
 
-app.get('/test', async (req, res) => {
-	return res.json(name)
-})
+app.get("/test", async (req, res) => {
+	return res.json(name);
+});
 
 module.exports = app;
