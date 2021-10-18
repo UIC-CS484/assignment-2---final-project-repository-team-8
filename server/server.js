ea1a884f206045cac4a544a3f404da7f574c5c43
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-18 14:11:27 -0500

    cleaned up error logic for login

--- server/server.js
+++ server/server.js
@@ -16,13 +16,13 @@ const port = process.env.NODE_ENV === "test" ? 8081 : 8080;
 const db = require("./database.js").db;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(
-	session({
-		secret: "secretcode",
-		resave: true,
-		saveUninitialized: true
-	})
+    session({
+        secret: "secretcode",
+        resave: true,
+        saveUninitialized: true
+    })
 );
 
 app.use(cookieParser("secretcode"));
@@ -34,83 +34,93 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 
 app.listen(port, () => {
-	console.log("Server running on port " + port);
+    console.log("Server running on port " + port);
 });
 
 const routes = {
-	REGISTER: "/account/register",
-	LOGIN: "/account/login",
-	ACCOUNT: "/account"
+    REGISTER: "/account/register",
+    LOGIN: "/account/login",
+    ACCOUNT: "/account"
 };
 
 app.post(routes.REGISTER, async (req, res) => {
-	const [ok, reason] = validRegistrationParameters(req.body.name, req.body.email, req.body.password);
-	if (!ok) {
-		console.log(reason);
-		res.status(StatusCodes.BAD_REQUEST).json({ error: reason });
-		return;
-	}
+    const [ok, reason] = validRegistrationParameters(req.body.name, req.body.email, req.body.password);
+    if (!ok) {
+        console.log(reason);
+        res.status(StatusCodes.BAD_REQUEST).json({error: reason});
+        return;
+    }
 
-	bcrypt.hash(req.body.password, saltRounds, function(hashErr, hash) {
-		if (hashErr) {
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: messages.REGISTRATION_FAILED });
-			return;
-		}
+    bcrypt.hash(req.body.password, saltRounds, function (hashErr, hash) {
+        if (hashErr) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: messages.REGISTRATION_FAILED});
+            return;
+        }
 
-		const params = [req.body.name, req.body.email, hash];
-		db.run(query.INSERT_ACCOUNT, params, (dbErr, row) => {
-			if (dbErr) {
-				res.status(StatusCodes.CONFLICT).json({ error: messages.EMAIL_ALREADY_EXISTS });
-			} else {
-				res.status(StatusCodes.OK).json({ message: messages.REGISTRATION_SUCCEEDED });
-			}
-		});
-	});
+        const params = [req.body.name, req.body.email, hash];
+        db.run(query.INSERT_ACCOUNT, params, (dbErr, row) => {
+            if (dbErr) {
+                res.status(StatusCodes.CONFLICT).json({error: messages.EMAIL_ALREADY_EXISTS});
+            } else {
+                res.status(StatusCodes.OK).json({message: messages.REGISTRATION_SUCCEEDED});
+            }
+        });
+    });
 });
 
 
 app.post(routes.LOGIN, async (req, res, next) => {
-	passport.authenticate("local", (err, user, info) => {
-		if (err === errors.USER_NOT_FOUND) {
-			res.status(StatusCodes.NOT_FOUND).json({ error: messages.USER_NOT_FOUND });
-		} else if (err === errors.INCORRECT_PASSWORD) {
-			res.status(StatusCodes.UNAUTHORIZED).json({ error: messages.INCORRECT_PASSWORD });
-		} else if (err) {
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: messages.UNEXPECTED_ERROR });
-		} else if (!user) {
-			res.status(StatusCodes.NOT_FOUND).json({ error: messages.USER_NOT_FOUND });
-		} else {
-			req.logIn(user, (err) => {
-				if (err) {
-					res.status(StatusCodes.UNAUTHORIZED).json({ error: messages.LOGIN_FAILED });
-				} else {
-					res.status(StatusCodes.OK).json({ message: messages.LOGIN_SUCCEEDED });
-				}
-			});
-		}
-	})(req, res, next);
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            let status, msg;
+            switch (err) {
+                case errors.USER_NOT_FOUND:
+                    status = StatusCodes.NOT_FOUND
+                    msg = messages.USER_NOT_FOUND
+                    break;
+                case errors.INCORRECT_PASSWORD:
+                    status = StatusCodes.UNAUTHORIZED
+                    msg = messages.INCORRECT_PASSWORD
+                    break;
+                default:
+                    status = StatusCodes.INTERNAL_SERVER_ERROR
+                    msg = messages.UNEXPECTED_ERROR
+            }
+            res.status(status).json({error: msg});
+        } else if (!user) {
+            res.status(StatusCodes.NOT_FOUND).json({error: messages.USER_NOT_FOUND});
+        } else {
+            req.logIn(user, (err) => {
+                if (err) {
+                    res.status(StatusCodes.UNAUTHORIZED).json({error: messages.LOGIN_FAILED});
+                } else {
+                    res.status(StatusCodes.OK).json({message: messages.LOGIN_SUCCEEDED});
+                }
+            });
+        }
+    })(req, res, next);
 });
 
 app.get(routes.ACCOUNT, (req, res) => {
-	if (!req.query.email) {
-		res.status(StatusCodes.BAD_REQUEST).json({ error: messages.BAD_REQUEST });
-	}
+    if (!req.query.email) {
+        res.status(StatusCodes.BAD_REQUEST).json({error: messages.BAD_REQUEST});
+    }
 
-	const params = [req.query.email];
-	db.get(query.GET_ACCOUNT, params, function(err, row) {
-		if (err) {
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
-			return console.error(err);
-		}
+    const params = [req.query.email];
+    db.get(query.GET_ACCOUNT, params, function (err, row) {
+        if (err) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error: err.message});
+            return console.error(err);
+        }
 
-		if (row) {
-			res.status(StatusCodes.OK).json(row);
-		} else {
-			res.status(StatusCodes.NOT_FOUND).json({
-				error: messages.USER_NOT_FOUND
-			});
-		}
-	});
+        if (row) {
+            res.status(StatusCodes.OK).json(row);
+        } else {
+            res.status(StatusCodes.NOT_FOUND).json({
+                error: messages.USER_NOT_FOUND
+            });
+        }
+    });
 });
 
 module.exports.app = app;
