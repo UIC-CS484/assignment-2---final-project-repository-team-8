d5825c6e8fffa0022162c61e7e7624754483d3ef
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-24 12:24:22 -0500

    added tweet button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.scss";
 import { Link, useHistory } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 
 export default function Home() {
 	const history = useHistory();
+	const [tweet, setTweet] = useState("");
 
 	const handleLogout = () => {
 		localStorage.clear();
 		history.push("/login");
-	}
+	};
+
+	const submitTweet = () => {
+		console.log(tweet);
+	};
 
 	return (
 		<div className="home">
 			Congrats! You logged in!!
-			<br/>
+			<br />
 			<button onClick={handleLogout}>logout</button>
+			<Form onSubmit={ev => {
+				ev.preventDefault();
+			}}>
+				<div className="redirect">
+					<Link to={"/login"}>Login</Link>
+				</div>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Tweet</Form.Label>
+					<Form.Control onChange={e => setTweet(e.target.value)} placeholder="Tweet" />
+				</Form.Group>
+
+				<Button onClick={submitTweet} block size="sm" type="submit">
+					Tweet
+				</Button>
+			</Form>
 		</div>
 	);
 }
