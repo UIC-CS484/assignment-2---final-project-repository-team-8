6039114dee3dd54f0932b966332275c8104a376a
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-11-09 17:34:06 -0600

    fixed route error

--- client/src/Common.js
+++ client/src/Common.js
@@ -1,25 +1,25 @@
 const routes = {
-	REGISTER: "/account/register",
-	LOGIN: "/account/login",
-	TWEET: "/tweet",
-	TWEETS_FROM_USER: "/tweets/use",
-	GET_ALL_TWEETS: "/tweets/all",
-	GET_WEATHER_API_KEY: "/api/weather"
+    REGISTER: "/account/register",
+    LOGIN: "/account/login",
+    TWEET: "/tweet",
+    TWEETS_FROM_USER: "/tweets/user/",
+    GET_ALL_TWEETS: "/tweets/all",
+    GET_WEATHER_API_KEY: "/api/weather"
 };
 
 const errors = {
-	NAME: "Please provide a name",
-	EMAIL: "Please provide an email",
-	PASSWORD: "Please provide an password",
-	WEATHER: "Please provide a valid city or address"
+    NAME: "Please provide a name",
+    EMAIL: "Please provide an email",
+    PASSWORD: "Please provide an password",
+    WEATHER: "Please provide a valid city or address"
 };
 
 const constants = {
-	EMAIL: "email",
-	NAME: "name",
-	TOKEN: "token",
-	SECRET: "Hello, world!",
-	EMPTY: ""
+    EMAIL: "email",
+    NAME: "name",
+    TOKEN: "token",
+    SECRET: "Hello, world!",
+    EMPTY: ""
 };
 
 module.exports.constants = constants;
