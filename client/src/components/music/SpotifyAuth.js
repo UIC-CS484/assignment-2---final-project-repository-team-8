005a855640f186d8abe2f7151ed996df8227b793
Farooq <syed@farooq.dev>
2021-11-12 15:30:39 -0600

    Implemented a spotify api with chart of the data

--- client/src/components/music/SpotifyAuth.js
+++ client/src/components/music/SpotifyAuth.js
@@ -1,21 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { routes } from "../../Common";
+import { routes, constants } from "../../Common";
 
 export default function SpotifyAuth(code) {
 
     const [accessToken, setAccessToken] = useState();
 
-    useEffect(() => {
-        axios.post(routes.SPOTIFY_LOGIN, { code })
-            .then((response) => {
-              setAccessToken(response.data.accessToken);
-              window.history.pushState({}, null, "/spotify");
-          })
-          .catch(() => {
-              window.location = "/home";
-          });
-      }, [code]);
+    
+        useEffect(() => {
+            if(localStorage.getItem(constants.SPOTIFY_TOKEN) === null){
+            axios.post(routes.SPOTIFY_LOGIN, { code })
+                .then((response) => {
+                  setAccessToken(response.data.accessToken);
+                  localStorage.setItem(constants.SPOTIFY_TOKEN, response.data.accessToken);
+                  window.history.pushState({}, null, "/spotify");
+              })
+              .catch(() => {
+                  window.location = "/home";
+              });
+            }else{
+                setAccessToken(localStorage.getItem(constants.SPOTIFY_TOKEN))
+            }
+        }, [code]);
+    
     
       return accessToken
 }
--- client/src/components/music/SpotifyDashboard.js
+++ client/src/components/music/SpotifyDashboard.js
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 import SpotifyAuth from './SpotifyAuth';
 import SpotifyPlayer from "./SpotifyPlayer";
 import SpotifyTrack from "./SpotifyTrack";
-import SpotifyArtistChart from "./SpotifyArtistChart";
 import { Form } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
 import "../style/Home.scss";
 import NavBar from "../NavBar";
 import axios from "axios";
+import Plot from 'react-plotly.js';
 
 import { constants, errors, routes } from "../../Common";
 
@@ -19,15 +19,30 @@ export default function SpotifyDashboard(body) {
     const [playingTrack, setPlayingTrack] = useState()
     const [lyrics, setLyrics] = useState("")
     const [topArtists, setTopArtists] = useState([]);
-    
-    
-    // const spotifyApi = new SpotifyWebApi({
-    //     clientId: process.env.SPOTIFY_API_ID,
-    //     clientSecret: process.env.SPOTIFY_API_SECRET
-    // })
 
     var spotifyApi = new SpotifyWebApi();
     spotifyApi.setAccessToken(accessToken);
+
+	// Transform track data 
+	function PopularityByTrack(data){
+
+		let plotData = [];
+
+		let names = [];
+		let popularity = [];
+        let count = 1;
+
+		data.map(each => {
+			names.push("("+count+") "+each.name);
+			popularity.push(each.popularity);
+            count +=1;
+		})
+
+		plotData['names'] = names;
+		plotData['popularity'] = popularity;
+
+		return plotData;
+	}
 
     function chooseTrack(track) {
         setPlayingTrack(track)
@@ -37,14 +52,23 @@ export default function SpotifyDashboard(body) {
     
     useEffect(() => {
         if (!playingTrack) return
-
         axios.get(routes.SPOTIFY_LYRICS, {
             params: {
                 track: playingTrack.title,
                 artist: playingTrack.artist,
             },})
             .then(res => {
-                setLyrics(res.data.lyrics)
+                setLyrics(res.data.lyrics);
+        })
+        
+        axios.get(routes.SPOTIFY_TOP_ARTIST, {
+            'headers': {
+                'Authorization': 'Bearer ' + accessToken //
+            }
+            }).then((res) => {
+                setTopArtists(res.data.items);
+            }).catch((error) => {
+                console.error(error);
             })
     }, [playingTrack])
 
@@ -61,15 +85,12 @@ export default function SpotifyDashboard(body) {
                 (smallest, image) => {
                     if (image.height < smallest.height) return image
                     return smallest
-                },
-                track.album.images[0]
-                )
-
+                }, track.album.images[0])
                 return {
-                artist: track.artists[0].name,
-                title: track.name,
-                uri: track.uri,
-                albumUrl: smallestAlbumImage.url,
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url,
                 }
             }))
         })
@@ -87,7 +108,6 @@ export default function SpotifyDashboard(body) {
             onChange={e => setSearch(e.target.value)}
         />
         <SpotifyPlayer accessToken={accessToken} trackUri={playingTrack?.uri} />
-        <SpotifyArtistChart accessToken={accessToken}  />
         <br />
         <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
             {searchResults.map(track => (
@@ -103,6 +123,63 @@ export default function SpotifyDashboard(body) {
                 </div>
             )}
         </div>
+        <br />
+        
+        <div>
+			<Plot 
+				data={[
+					{
+						type: 'bar',
+						x: PopularityByTrack(topArtists)['names'],
+						y: PopularityByTrack(topArtists)['popularity'],
+						marker: {color:'#00000'}
+					}
+				]}
+				layout={{
+					width: 500, 
+					height: 600, 
+					title: 'Your top 8 Artists',
+					title: '<b>Your top 8 Artists</b>',
+					margin:{
+						l: 40,
+						r: 40,
+						t: 90,
+                        b: 100
+					},
+					paper_bgcolor: '#cfe0fc',
+					plot_bgcolor: '#cfe0fc',
+					font: {
+						family: 'Newsreader, serif',
+						size: 20,
+						color: '#236B8E'
+					},
+					xaxis: {
+						showticklabels: true,
+						tickfont:{
+							family: 'Arial, sans-serif',
+							size: 12,
+							color: '#236B8E'
+						}
+					},
+					yaxis: {
+						title: 'Popularity of Artist',
+						titlefont: {
+							family: 'Arial, sans-serif',
+							size: 12,
+							color: '#236B8E'
+						},
+						showticklabels: true,
+						tickfont: {
+							family: 'Arial, sans-serif',
+							size: 12,
+							color: '#236B8E'
+						}
+					},
+					hovermode: 'closest'
+				}}
+			/>
+		</div>
+        <p><center> Select a song to look at data</center></p>
     </div>
     )
 }
